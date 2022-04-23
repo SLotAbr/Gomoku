c52395a4c1ae1be2f919b4ba23808a0c18df0605
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2022-04-23 05:49:37 +0300

    horizontal_check

--- gomoku_logic.cpp
+++ gomoku_logic.cpp
@@ -37,10 +37,47 @@ extern "C" int vertical_check(int last_move[2], int field_info[YSIZE*XSIZE])
 		return 0;
 }
 
+extern "C" int horizontal_check(int last_move[2], int field_info[YSIZE*XSIZE])
+{
+	const int x = last_move[0] - 4;
+	const int y = last_move[1];
+
+	int player_id = 0;
+	int line_length = 0;
+
+	for (int i = 0; i < 9; ++i)
+	{
+		if ((x+i) < 0) continue;
+		if ((x+i) >= YSIZE) break;
+
+		int cell = field_info[y*YSIZE + (x+i)];
+
+		if (cell != 0) {
+			if (cell == player_id) line_length++;
+			else {
+				player_id = cell;
+				line_length = 1;
+			}
+		} else {
+			player_id = 0;
+			line_length = 0;
+		}
+		
+		if (line_length == 5) break;
+	}
+
+	if (line_length==5)
+		return player_id;
+	else
+		return 0;
+}
+
 extern "C" int find_winner(int last_player, int last_move[2], int field_info[YSIZE*XSIZE])
 {
 	// vertical
-	return vertical_check(last_move, field_info);
+	int winner = vertical_check(last_move, field_info);
+	if (winner!=0) return winner;
+	else return horizontal_check(last_move, field_info);
 
 	// horizontal
 	// main diagonal
